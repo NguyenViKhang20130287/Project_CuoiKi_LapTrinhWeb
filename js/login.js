6e29e49f7e1472f003240c956ab4e5038141d295
Nguyễn Phi Khanh <20130288@st.hcmuaf.edu.vn>
2022-10-30 19:32:59 +0700

    add close button to register form

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ var registerBtn = document.querySelector('#register-button-login')
 var loginBtn = document.querySelector('#login-button-register')
 var registerModal = document.querySelector('.modal')
 var registerModalContent = document.querySelector('.modal-content')
-
+var close = document.getElementById('closeButton');
 var modal = document.querySelector('.modal')
 registerBtn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -32,9 +32,17 @@ loginBtn.addEventListener('click', function (event) {
     registerModalContent.style.transform = 'scale(.5)';
     registerModalContent.style.opacity = '0';
 })
+close.addEventListener('click', function (event) {
+    event.preventDefault();
+    registerModal.style.visibility = 'hidden';
+    registerModal.style.opacity = '0';
+    registerModalContent.style.visibility = 'hidden';
+    registerModalContent.style.transform = 'scale(.5)';
+    registerModalContent.style.opacity = '0';
+})
 
 window.addEventListener('click', function (event) {
-    if(event.target == modal) {
+    if (event.target == modal) {
         registerModal.style.visibility = 'hidden';
         registerModal.style.opacity = '0';
         registerModalContent.style.visibility = 'hidden';
